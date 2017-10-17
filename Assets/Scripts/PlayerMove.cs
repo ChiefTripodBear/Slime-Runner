ac366d8c126b9d92a8e723818ec600864882c579
Adrian V <avalacos@gmail.com>
2017-10-17 23:27:26 +1100

    Player Input (WIP)
    need to align rotator and player position in update, so the player
    doesn't end up rotating around the rotator position, and instead turns
    on the spot.

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float m_forwardSpeed = 20f;
+    public float m_backSpeed = 15f;
+ 
+void Update()
+    {
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            transform.localPosition += new Vector3(0f, 0f, Input.GetAxis("Vertical") * m_forwardSpeed * Time.deltaTime);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            transform.localPosition += new Vector3(0f, 0f, Input.GetAxis("Vertical") * m_backSpeed * Time.deltaTime);
+        }     
+    }
+
+}
---
+++ Assets/Scripts/PlayerRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    public float m_turnSpeed = 200f;
+
+    void Update ()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            transform.Rotate(0f, Input.GetAxis("Horizontal") * m_turnSpeed * Time.deltaTime, 0f);
+        }
+
+    }
+}
