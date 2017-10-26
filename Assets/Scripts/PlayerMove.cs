bd1617f6bfb7adcb4d786547ce17d15da6c158d3
Adrian V <avalacos@gmail.com>
2017-10-26 22:21:47 +1100

    Player movement working, hex tilemap spawns
    will continue working on hexes over next few days.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,17 +6,23 @@ public class PlayerMove : MonoBehaviour
 {
     public float m_forwardSpeed = 20f;
     public float m_backSpeed = 15f;
- 
-void Update()
+    public float m_turnSpeed = 200f;
+
+    void Update()
     {
         if (Input.GetAxis("Vertical") > 0)
         {
-            transform.localPosition += new Vector3(0f, 0f, Input.GetAxis("Vertical") * m_forwardSpeed * Time.deltaTime);
+            transform.position += transform.forward * Input.GetAxis("Vertical") * m_forwardSpeed * Time.deltaTime;
         }
         else if (Input.GetAxis("Vertical") < 0)
         {
-            transform.localPosition += new Vector3(0f, 0f, Input.GetAxis("Vertical") * m_backSpeed * Time.deltaTime);
-        }     
+            transform.position += transform.forward * Input.GetAxis("Vertical") * m_backSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            transform.Rotate(0f, Input.GetAxis("Horizontal") * m_turnSpeed * Time.deltaTime, 0f);
+        }
     }
 
 }
