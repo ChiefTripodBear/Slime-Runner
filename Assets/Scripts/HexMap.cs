d6090c2b7b73b3c5233f5dfd0c81a4d153a79a42
Adrian V <avalacos@gmail.com>
2017-11-08 09:52:09 +1100

    Random Hex colours

--- Assets/Scripts/HexMap.cs
+++ Assets/Scripts/HexMap.cs
@@ -12,15 +12,29 @@ public class HexMap : MonoBehaviour {
 
     public GameObject HexPrefab;
 
+    public Material[] HexMaterials;
+
+    int mapHeight = 60;
+    int mapWidth = 20;
+
     public void GenerateMap()
     {
-        for (int column = 0; column < 10; column++)
+        for (int column = 0; column < mapHeight; column++)
         {
-            for (int row = 0; row < 10; row++)
+            for (int row = 0; row < mapWidth; row++)
             {
                 Hex h = new Hex(column, row);
 
-                Instantiate(HexPrefab, h.Position(), Quaternion.identity, this.transform);
+                GameObject hexGO = (GameObject)Instantiate
+                    (
+                    HexPrefab, 
+                    h.Position(), 
+                    Quaternion.identity, 
+                    this.transform
+                    );
+
+                MeshRenderer mr = hexGO.GetComponentInChildren<MeshRenderer>();
+                mr.material = HexMaterials[Random.Range(0, HexMaterials.Length)];
             }
         }
     }
